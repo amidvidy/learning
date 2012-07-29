f09c6643fbf0a0b771574d55e0d39eb68b9dac1b
Adam Midvidy <amidvidy@gmail.com>
2012-07-29 17:43:22 -0400

    commented out main

--- algorithms-in-c/fundamentals/euclid.c
+++ algorithms-in-c/fundamentals/euclid.c
@@ -13,10 +13,10 @@ int gcd(int u, int v) {
     return v;
 }
 
-int main(void) {
-    int x, y;
-    while (scanf("%d %d", &x, &y) != EOF) {
-        if (x > 0 && y > 0)
-            printf("%d %d %d\n", x, y, gcd(x, y));
-    }
-}
+/* int main(void) { */
+/*    int x, y; */
+/*     while (scanf("%d %d", &x, &y) != EOF) { */
+/*         if (x > 0 && y > 0) */
+/*             printf("%d %d %d\n", x, y, gcd(x, y)); */
+/*     } */
+/* } */
