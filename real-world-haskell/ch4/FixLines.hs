11796b85b00dd2ecce1b29b6d815d55a2095ccbb
Adam Midvidy <amidvidy@gmail.com>
2012-08-15 14:51:51 -0700

    more progress on RWH

---
+++ real-world-haskell/ch4/FixLines.hs
@@ -0,0 +1,27 @@
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+             input <- readFile inputFile
+             writeFile outputFile (function input)
+
+main = mainWith myFunction
+     where mainWith function = do
+             args <- getArgs
+             case args of
+               [input, output] -> interactWith function input output
+               _ -> putStrLn "error: exactly two arguments needed"
+
+
+myFunction = unlines . splitLines
+
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminator cs
+    in pre : case suf of
+               ('\r':'\n':rest) -> splitLines rest
+               ('\r':rest)      -> splitLines rest
+               ('\n':rest)      -> splitLines rest
+               _                -> []
+
+isLineTerminator c = c == '\r' || c == '\n'
---
+++ real-world-haskell/ch4/SplitLines.hs
@@ -0,0 +1,11 @@
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+    let (pre, suf) = break isLineTerminator cs
+    in pre : case suf of
+               ('\r':'\n':rest) -> splitLines rest
+               ('\r':rest)      -> splitLines rest
+               ('\n':rest)      -> splitLines rest
+               _                -> []
+
+isLineTerminator c = c == '\r' || c == '\n'
