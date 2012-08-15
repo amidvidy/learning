74a801f12a5760d8752da8057ab087ef269cdb76
Adam Midvidy <amidvidy@gmail.com>
2012-08-15 15:14:53 -0700

    added myListFunctions

---
+++ real-world-haskell/ch4/myListFunctions.hs
@@ -0,0 +1,34 @@
+-- same as 'length'
+myLength :: [a] -> Int
+myLength (x:xs) = 1 + myLength xs
+myLength _ = 0
+
+-- same as 'null'
+myNull :: [a] -> Bool
+myNull [] = True
+myNull _ = False
+
+-- same as 'head'
+myHead :: [a] -> a
+myHead (x:xs) = x
+myHead _ = error "myHead takes a non-empty list"
+
+mySafeHead :: [a] -> Maybe a
+mySafeHead (x:xs) = Just x
+mySafeHead _ = Nothing
+
+-- same as 'tail'
+myTail :: [a] -> [a]
+myTail (x:xs) = xs
+myTail _ = error "myTail takes a non-empty list"
+
+-- same as 'last'
+myLast :: [a] -> a
+myLast [x] = x
+myLast (x:xs) = myLast xs
+
+-- same as 'init'
+myInit :: [a] -> [a]
+myInit [x] = []
+myInit (x:xs) = x : myInit xs
+
